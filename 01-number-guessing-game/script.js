d5c803b7330f9f57df2161bd17a40f1643311dc2
Aditya Agarwal <adityaagarwal382@gmail.com>
2026-02-13 08:04:03 +0530

    Implement number guessing game logic
    Add js code for logic

--- 01-number-guessing-game/script.js
+++ 01-number-guessing-game/script.js
@@ -1,1 +1,19 @@
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log(randomNumber)
+
+let input = document.getElementbyId("guessInput");
+let button = document.getElementbyId("button");
+let message = document.getElementsbyId("message");
+
+button.addEventListener("click", function () {
+  let userGuess = Number(input.value);
+
+  if (userGuess === randomNumber) {
+    message.innerText = "Correct! 🎉";
+  } else if (userGuess > randomNumber) {
+    message.innerText = "Too high!";
+  } else {
+    message.innerText = "Too low!";
+  }
+});
 
